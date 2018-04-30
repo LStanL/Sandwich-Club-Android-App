4438d77cac761749e562c8b516f285c49ffdb27a
Andrei Stasevici <andreistasevici@gmail.com>
2018-04-29 22:45:38 -0700

    Populating details view UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -50,7 +51,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -63,7 +64,17 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView alsoKnownAs = findViewById(R.id.also_known_tv);
+        alsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
 
+        TextView description = findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
+
+        TextView ingridients = findViewById(R.id.ingredients_tv);
+        ingridients.setText(sandwich.getIngredients().toString());
+
+        TextView origin = findViewById(R.id.origin_tv);
+        origin.setText(sandwich.getPlaceOfOrigin());
     }
 }
